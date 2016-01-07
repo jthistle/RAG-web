55896458fd07fc44c12bf4d9391c258cfc1d69f5
jthistle <admin@jamesthistlewood.co.uk>
2016-01-07 17:24:51 +0000

    Add certain text selection and bugfixes
    You can now select comments containing certain text - useful for
    giveaways with multiple different games.

--- index.php
+++ index.php
@@ -9,8 +9,9 @@
 <body>
   <h1>/r/RandomActsOfGaming giveaway decider</h1>
   <form method="POST" action='index.php'>
-    <label>Enter the bit after /comments/ in the URL of your giveaway: <input name='submissionID' type='text' value='<?php strip_tags($_POST["submissionID"]); ?>' /></label><br />
-    <label>Enter the upper bound of your random number (leave blank to choose random comment): <input name='upperBound' type='text' value='<?php strip_tags($_POST["upperBound"]); ?>' /></label>
+    <label>Enter the bit after /comments/ in the URL of your giveaway, e.g. 3ab4cd: <input name='submissionID' type='text' value='<?php echo strip_tags($_POST["submissionID"]); ?>' /></label><br />
+    <label>Enter the upper bound of your random number (leave blank to choose random comment): <input name='upperBound' type='text' value='<?php echo strip_tags($_POST["upperBound"]); ?>' /></label><br />
+    <label>Enter text required in comment (leave blank to accept any text): <input name='textToFind' type='text' value='<?php echo strip_tags($_POST["textToFind"]); ?>' /></label>
     <p></p><input type='submit' name='getWinner' value='Choose a winner!' />
   </form>
   <?php
@@ -27,10 +28,12 @@
         // $comments[1]["data"]["children"] // these are replies to main post
         $commentsParsed = Array();
         $upperBound = intval(strip_tags($_POST['upperBound']));
+        $textToFind = strip_tags($_POST['textToFind']);
         $randNumber = rand(1, $upperBound);
         
         for ($i=0;$i<count($comments[1]["data"]["children"]);$i++){
-          $commentWords = $result = preg_replace("/[^a-zA-Z0-9]+/", "", explode(" ", strip_tags(html_entity_decode($comments[1]["data"]["children"][$i]["data"]["body_html"]))));
+          $commentText = strip_tags(html_entity_decode($comments[1]["data"]["children"][$i]["data"]["body_html"]));
+          $commentWords = preg_replace("/[^a-zA-Z0-9]+/", "", explode(" ", $commentText));
           $number = "";
           foreach($commentWords as $commentWord){
             if (is_numeric($commentWord)){
@@ -38,20 +41,36 @@
               break;
             }
           }
-          $commentsParsed[] = [strip_tags(html_entity_decode($comments[1]["data"]["children"][$i]["data"]["body_html"])), $comments[1]["data"]["children"][$i]["data"]["author"], $number];
+          if ($textToFind != ""){ // is there certain text to find in the comment?
+            $alphanumeric = preg_replace('/[^a-zA-Z0-9\s]/', "", $commentText);
+            if (strpos(strtolower($alphanumeric), strtolower($textToFind)) !== FALSE){ // text exists in comment, add to parsed
+              $commentsParsed[] = [strip_tags(html_entity_decode($comments[1]["data"]["children"][$i]["data"]["body_html"])), $comments[1]["data"]["children"][$i]["data"]["author"], $number];
+            }
+          } else{
+            $commentsParsed[] = [strip_tags(html_entity_decode($comments[1]["data"]["children"][$i]["data"]["body_html"])), $comments[1]["data"]["children"][$i]["data"]["author"], $number];
+          }
           // ["text", "author", "number"]
         }
-		    $winners = []; // prox, username, body text
+		    $winners = []; // prox, username, body text, spot on
         foreach ($commentsParsed as $comment){
           //echo("checking comment: ".$comment[0].", prox = ".abs($comment[2] - $randNumber).)
           if (count($winners) == 0){
-            $winners[] = [abs($comment[2] - $randNumber), $comment[1], $comment[0]]; // no winners yet, add first one
+            if ($comment[2] == $randNumber) $spotOn = true;
+            else $spotOn = false;
+            
+            $winners[] = [abs($comment[2] - $randNumber), $comment[1], $comment[0], $spotOn]; // no winners yet, add first one
           } else{
             if (abs($comment[2] - $randNumber) < $winners[0][0]){ // more of a winner?
+              if ($comment[2] == $randNumber) $spotOn = true;
+              else $spotOn = false;
+              
               $winners = []; // clear current list
-              $winners[] = [abs($comment[2] - $randNumber), $comment[1], $comment[0]]; // add to new list
+              $winners[] = [abs($comment[2] - $randNumber), $comment[1], $comment[0], $spotOn]; // add to new list
             } else if (abs($comment[2] - $randNumber) == $winners[0][0]){
-              $winners[] = [abs($comment[2] - $randNumber), $comment[1], $comment[0]]; // add to current winners list
+              if ($comment[2] == $randNumber) $spotOn = true;
+              else $spotOn = false;
+              
+              $winners[] = [abs($comment[2] - $randNumber), $comment[1], $comment[0], $spotOn]; // add to current winners list
             }
           }
         }
@@ -63,16 +82,21 @@
           $winnerUname = $winner[1];
           $winnerBody = $winner[2];
           $hadToDecide = true;
+          $winnerSpotOn = $winners[3];
         } else{
           $winnerUname = $winners[0][1];
           $winnerBody = $winners[0][2];
+          $winnerSpotOn = $winners[0][3];
         }
+        
+        if ($winnerSpotOn) $spotOn = " was spot on";
+        else $closest = "The closest person was ";
         
         echo "<p>The number is $randNumber</p>";
         
         if ($hadToDecide) echo "<p>".count($winners)." people were the same proximity to the number, so one was randomly chosen</p>";
         
-        echo "<p>The closest person was /u/$winnerUname with this text: </p>";
+        echo "<p>$closest/u/$winnerUname$spotOn with this text: </p>";
         
         echo "<p class='quote'>\"$winnerBody\"</p>";
         
@@ -80,9 +104,23 @@
         
         echo "<br />Reload the page to get another winner!<br />There are $commentCount comments total (if this doesn't match with the reddit comment count, it means some comments have been deleted).";
       } else if ($_POST['upperbound'] == ""){
-        $randNum = rand(0,count($comments[1]["data"]["children"])-1);
-        $bodyText = strip_tags(html_entity_decode($comments[1]["data"]["children"][$randNum]["data"]["body_html"]));
-        $author = strip_tags(html_entity_decode($comments[1]["data"]["children"][$randNum]["data"]["author"]));
+        $textToFind = strip_tags($_POST['textToFind']);
+        if ($textToFind != ""){
+          $allComments = $comments[1]["data"]["children"];
+          $chosenComments = [];
+          foreach ($allComments as $comment){
+            if (strpos(strtolower(preg_replace('/[^a-zA-Z0-9\s]/', "", strip_tags(html_entity_decode($comment['data']['body_html'])))), strtolower($textToFind)) !== False){
+              $chosenComments[] = [strip_tags(html_entity_decode($comment['data']['body_html'])), $comment['data']['author']];
+            }
+          }
+          $randNum = rand(0,count($chosenComments)-1);
+          $bodyText = $chosenComments[$randNum][0];
+          $author = $chosenComments[$randNum][1];
+        } else{
+          $randNum = rand(0,count($comments[1]["data"]["children"])-1);
+          $bodyText = strip_tags(html_entity_decode($comments[1]["data"]["children"][$randNum]["data"]["body_html"]));
+          $author = strip_tags(html_entity_decode($comments[1]["data"]["children"][$randNum]["data"]["author"]));
+        }
         echo "<p>The chosen person was /u/$author with this text: </p>";
         
         echo "<p class='quote'>\"$bodyText\"</p>";
