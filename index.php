8613fc2ea83103d659e3bc82105ae0d79a75f862
jthistle <admin@jamesthistlewood.co.uk>
2016-05-10 19:16:35 +0100

    Add ability to choose accounts with minimum account age

--- index.php
+++ index.php
@@ -2,19 +2,22 @@
 
 <html>
 <head>
-  <title>/r/RandomActsOfGaming giveaway decider</title>
+  <title>Reddit giveaway decider</title>
   <link rel='stylesheet' href='style.css' />
 </head>
 
 <body>
-  <h1>/r/RandomActsOfGaming giveaway decider</h1>
+  <h1>Reddit giveaway decider</h1>
   <form method="POST" action='index.php'>
     <label>Enter the bit after /comments/ in the URL of your giveaway, e.g. 3ab4cd: <input name='submissionID' type='text' value='<?php echo strip_tags($_POST["submissionID"]); ?>' placeholder="e.g. 3ab4cd" /></label><br />
     <label>Enter the upper bound of your random number (leave blank to choose random comment): <input name='upperBound' type='text' value='<?php echo strip_tags($_POST["upperBound"]); ?>' /></label><br />
-    <label>Enter text required in comment (leave blank to accept any text): <input name='textToFind' type='text' value='<?php echo strip_tags($_POST["textToFind"]); ?>' /></label>
+	<label>Enter text required in comment (leave blank to accept any text): <input name='textToFind' type='text' value='<?php echo strip_tags($_POST["textToFind"]); ?>' /></label><br />
+	<label>Enter mininum account age in days (leave blank to accept any age): <input name='accountAge' type='number' value='<?php echo strip_tags($_POST["accountAge"]); ?>' /></label>
     <p></p><input type='submit' name='getWinner' value='Choose a winner!' />
   </form>
   <?php
+    //error_reporting(E_ALL);
+  
     if (isset($_POST['getWinner'])){
       require_once("Phapper/src/phapper.php");
       $r = new \Phapper\Phapper; // initialize PHP wrapper
@@ -31,7 +34,7 @@
         $textToFind = strip_tags($_POST['textToFind']);
         $randNumber = rand(1, $upperBound);
         
-        for ($i=0;$i<count($comments[1]["data"]["children"]);$i++){
+        for ($i=0;$i<count($comments[1]["data"]["children"]);$i++){ // parse comments and add to list of parsed comments
           $commentText = strip_tags(html_entity_decode($comments[1]["data"]["children"][$i]["data"]["body_html"]));
           $commentWords = preg_replace("/[^a-zA-Z0-9]+/", "", explode(" ", $commentText));
           $number = "";
@@ -51,58 +54,76 @@
           }
           // ["text", "author", "number"]
         }
-		    $winners = []; // prox, username, body text, spot on
-        foreach ($commentsParsed as $comment){
-          //echo("checking comment: ".$comment[0].", prox = ".abs($comment[2] - $randNumber).)
-          if (count($winners) == 0){
-            if ($comment[2] == $randNumber) $spotOn = true;
-            else $spotOn = false;
-            
-            $winners[] = [abs($comment[2] - $randNumber), $comment[1], $comment[0], $spotOn]; // no winners yet, add first one
-          } else{
-            if (abs($comment[2] - $randNumber) < $winners[0][0]){ // more of a winner?
-              if ($comment[2] == $randNumber) $spotOn = true;
-              else $spotOn = false;
-              
-              $winners = []; // clear current list
-              $winners[] = [abs($comment[2] - $randNumber), $comment[1], $comment[0], $spotOn]; // add to new list
-            } else if (abs($comment[2] - $randNumber) == $winners[0][0]){
-              if ($comment[2] == $randNumber) $spotOn = true;
-              else $spotOn = false;
-              
-              $winners[] = [abs($comment[2] - $randNumber), $comment[1], $comment[0], $spotOn]; // add to current winners list
+		
+        $winners = []; // prox, username, body text, spot on
+            foreach ($commentsParsed as $comment){
+              //echo("checking comment: ".$comment[0].", prox = ".abs($comment[2] - $randNumber).)
+              if (count($winners) == 0){
+                if ($comment[2] == $randNumber) $spotOn = true;
+                else $spotOn = false;
+                
+                $winners[] = [abs($comment[2] - $randNumber), $comment[1], $comment[0], $spotOn]; // no winners yet, add first one
+              } else{
+                if (abs($comment[2] - $randNumber) < $winners[0][0]){ // more of a winner?
+                  if ($comment[2] == $randNumber) $spotOn = true;
+                  else $spotOn = false;
+                  
+                  $winners = []; // clear current list
+                  $winners[] = [abs($comment[2] - $randNumber), $comment[1], $comment[0], $spotOn]; // add to new list
+                } else if (abs($comment[2] - $randNumber) == $winners[0][0]){
+                  if ($comment[2] == $randNumber) $spotOn = true;
+                  else $spotOn = false;
+                  
+                  $winners[] = [abs($comment[2] - $randNumber), $comment[1], $comment[0], $spotOn]; // add to current winners list
+                }
+              }
             }
+            $commentCount = count($comments[1]["data"]["children"]);
+            
+            if (count($winners) > 1){
+              $winner = $winners[array_rand($winners)];
+              //echo "<p>".json_encode($winner)."</p>";
+              $winnerUname = $winner[1];
+              $winnerBody = $winner[2];
+              $hadToDecide = true;
+              $winnerSpotOn = $winners[3];
+            } else{
+              $winnerUname = $winners[0][1];
+              $winnerBody = $winners[0][2];
+              $winnerSpotOn = $winners[0][3];
+            }
+        
+        $accountOldEnough = true;
+        
+        if ($_POST['accountAge'] != ""){
+          // check account age
+          $userDetails = json_decode(json_encode($r->getUser($winnerUname)), true); 
+          $accountAge = $userDetails["data"]["created_utc"];
+          if ( time() - ($_POST['accountAge']*(60*60*24)) > $accountAge){
+            // account is old enough
+          } else{
+            $accountOldEnough = false;
           }
         }
-        $commentCount = count($comments[1]["data"]["children"]);
-        
-        if (count($winners) > 1){
-          $winner = $winners[array_rand($winners)];
-          //echo "<p>".json_encode($winner)."</p>";
-          $winnerUname = $winner[1];
-          $winnerBody = $winner[2];
-          $hadToDecide = true;
-          $winnerSpotOn = $winners[3];
+            
+        if ($accountOldEnough){
+              if ($winnerSpotOn) $spotOn = " was spot on";
+              else $closest = "The closest person was ";
+              
+              echo "<p>The number is $randNumber</p>";
+              
+              if ($hadToDecide) echo "<p>".count($winners)." people were the same proximity to the number, so one was randomly chosen</p>";
+            
+              echo "<p>$closest/u/$winnerUname$spotOn with this text: </p>";
+            
+              echo "<p class='quote'>\"$winnerBody\"</p>";
+            
+              echo "<p><a href='https://www.reddit.com/message/compose/?to=$winnerUname' target='_blank'>Tell them about it now!</a></p>";
+            
+              echo "<br />Reload the page to get another winner!<br />There are $commentCount comments total (if this doesn't match with the reddit comment count, it means some comments have been deleted).";
         } else{
-          $winnerUname = $winners[0][1];
-          $winnerBody = $winners[0][2];
-          $winnerSpotOn = $winners[0][3];
+          echo "<p>A winner was chosen (/u/$winnerUname), but their account age was below the minimum specified.</p><p>Please pick again (sorry about this - this is to minimise reddit API calls)</p>";
         }
-        
-        if ($winnerSpotOn) $spotOn = " was spot on";
-        else $closest = "The closest person was ";
-        
-        echo "<p>The number is $randNumber</p>";
-        
-        if ($hadToDecide) echo "<p>".count($winners)." people were the same proximity to the number, so one was randomly chosen</p>";
-        
-        echo "<p>$closest/u/$winnerUname$spotOn with this text: </p>";
-        
-        echo "<p class='quote'>\"$winnerBody\"</p>";
-        
-        echo "<p><a href='https://www.reddit.com/message/compose/?to=$winnerUname' target='_blank'>Tell them about it now!</a></p>";
-        
-        echo "<br />Reload the page to get another winner!<br />There are $commentCount comments total (if this doesn't match with the reddit comment count, it means some comments have been deleted).";
       } else if ($_POST['upperbound'] == ""){
         $textToFind = strip_tags($_POST['textToFind']);
         if ($textToFind != ""){
@@ -121,16 +142,36 @@
           $bodyText = strip_tags(html_entity_decode($comments[1]["data"]["children"][$randNum]["data"]["body_html"]));
           $author = strip_tags(html_entity_decode($comments[1]["data"]["children"][$randNum]["data"]["author"]));
         }
-        echo "<p>The chosen person was /u/$author with this text: </p>";
-        
-        echo "<p class='quote'>\"$bodyText\"</p>";
-        
-        echo "<p><a href='https://www.reddit.com/message/compose/?to=$author' target='_blank'>Tell them about it now!</a></p>";
+		  
+		$accountOldEnough = true;
+		
+		if ($_POST['accountAge'] != ""){
+		  // check account age
+		  $userDetails = json_decode(json_encode($r->getUser($author)), true); 
+		  $accountAge = $userDetails["data"]["created_utc"];
+	    if ( time() - ($_POST['accountAge']*(60*60*24)) > $accountAge){
+			  // account is old enough
+		  } else{
+		    $accountOldEnough = false;
+		  }
 	    }
+        
+		if ($accountOldEnough){
+		  
+          echo "<p>The chosen person was /u/$author with this text: </p>";
+        
+          echo "<p class='quote'>\"$bodyText\"</p>";
+        
+          echo "<p><a href='https://www.reddit.com/message/compose/?to=$author' target='_blank'>Tell them about it now!</a></p>";
+		} else{
+		  echo "<p>A winner was chosen (/u/$author), but their account age was below the minimum specified.</p><p>Please pick again (sorry about this - this is to minimise reddit API calls)</p>";
+		}
+	  }
     }
   ?>
   <p><br /></p>
   <hr />
+	<p style="font-size: 12pt; color: black;">NOTICE: When giving away games with a number in the title (e.g. Fallout 4) ask for participants to use Roman numerals (e.g. Fallout IV) to avoid messing up the comment parser.</p>
   <p style="font-size: 8pt; color: grey;">This tool is completely open source - <a href="http://github.com/jthistle/rag-web/">find the code here</a></p>
   <p style="font-size: 8pt; color: grey;">Enjoy using this tool? Please leave a link to it in your giveaway - it really helps others looking for something like it!</p>
   <!-- Start of StatCounter Code for Default Guide -->
